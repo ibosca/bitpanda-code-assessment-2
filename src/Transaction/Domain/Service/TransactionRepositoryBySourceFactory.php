3eb64a8042c4cd33e3f28e42d63872214d8b237f
Isaac Boscà <isaac.bosca@exoticca.com>
2022-01-24 21:03:01 +0100

    Factory implementation

--- src/Transaction/Domain/Service/TransactionRepositoryBySourceFactory.php
+++ src/Transaction/Domain/Service/TransactionRepositoryBySourceFactory.php
@@ -4,15 +4,27 @@
 namespace Src\Transaction\Domain\Service;
 
 
+use RuntimeException;
 use Src\Transaction\Domain\Repository\TransactionRepository;
 use Src\Transaction\Domain\ValueObject\TransactionSource;
+use Src\Transaction\Infrastructure\Persistence\Csv\Repository\CsvTransactionRepository;
+use Src\Transaction\Infrastructure\Persistence\Database\Repository\DatabaseTransactionRepository;
 
 class TransactionRepositoryBySourceFactory
 {
+    public function __construct(
+        private DatabaseTransactionRepository $databaseTransactionRepository,
+        private CsvTransactionRepository $csvTransactionRepository
+    ){}
 
     public function __invoke(TransactionSource $source): TransactionRepository
     {
-        // TODO: Implement __invoke() method.
+
+        return match ($source->value()) {
+            TransactionSource::TRANSACTION_SOURCE_DB => $this->databaseTransactionRepository,
+            TransactionSource::TRANSACTION_SOURCE_CSV => $this->csvTransactionRepository,
+        };
+
     }
 
 }
